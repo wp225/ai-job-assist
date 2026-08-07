31183f4f99abdbd652f445e04646b038fcfb22d1
Jeorge Joshi <georgejoshi10@gmail.com>
2026-08-07 14:44:30 -0500

    Project Restructure

--- src/ai_job_assist/core/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field, ConfigDict
-
-class Settings(BaseSettings):
-    model_config = ConfigDict()
-    ANTHROPIC_KEY: str = Field()
-    DEBUG: bool = False
-    
-settings = Settings()
